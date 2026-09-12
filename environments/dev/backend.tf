64e0a0188de33040a656c177cd1fdeda7aa5fd17
Emmanuel Mulenga <emmanuelmulenga1207@gmail.com>
2026-09-12 10:58:20 +0200

    fix: add terraform required_version to environments/dev/backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0"
+
   backend "s3" {
     bucket              = "REPLACE-ME-dev-terraform-state"
     key                 = "devsecops-pipeline/dev/terraform.tfstate"
