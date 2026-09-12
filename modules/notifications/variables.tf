4456e499651effd424f7a9635a8d5f3584b2aee8
Emmanuel Mulenga <emmanuelmulenga1207@gmail.com>
2026-09-12 10:26:51 +0200

    fix: add Lambda VPC, code signing, and update log retention to 365 days (CKV_AWS_338, CKV_AWS_272, CKV_AWS_117)"

--- modules/notifications/variables.tf
+++ modules/notifications/variables.tf
@@ -28,7 +28,8 @@ variable "slack_webhook_secret_arn" {
 
 variable "log_retention_days" {
   type    = number
-  default = 90
+  default = 365
+  description = "CloudWatch log retention period in days. Must be at least 365 to comply with CKV_AWS_338."
 }
 
 variable "account_id" {
@@ -37,4 +38,22 @@ variable "account_id" {
 
 variable "region" {
   type = string
+}
+
+variable "lambda_subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "Subnet IDs for Lambda VPC configuration"
+}
+
+variable "lambda_security_group_ids" {
+  type        = list(string)
+  default     = []
+  description = "Security group IDs for Lambda VPC configuration"
+}
+
+variable "lambda_code_signing_config_arn" {
+  type        = string
+  default     = ""
+  description = "ARN of Lambda code signing configuration"
 }
